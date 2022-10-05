256adb76b261e483a9f45382594c45681e488983
tmanisin <115108443+tmanisin@users.noreply.github.com>
2022-10-06 00:12:03 +0530

    Update ipv6_structure.c

--- ipv6_structure.c
+++ ipv6_structure.c
@@ -1,10 +1,14 @@
-struct in6_addr {
- uint8_t s6_addr[16];
-};
+// Main IPv6 Structure
 struct sockaddr_in6 {
  uint8_t sin6_len;
  sa_family_t sin6_family;
  in_port_t sin6_port;
  uint32_t sin6_flowinfo;
  struct in6_addr sin6_addr;
+};
+
+// Supporting Function
+
+struct in6_addr {
+ uint8_t s6_addr[16];
 };
