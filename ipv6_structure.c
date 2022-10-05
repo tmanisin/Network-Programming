68c72331ae9803848b3bc5098c3b6dd504c60aea
tmanisin <115108443+tmanisin@users.noreply.github.com>
2022-10-06 00:10:12 +0530

    Create ipv6_structure.c

---
+++ ipv6_structure.c
@@ -0,0 +1,10 @@
+struct in6_addr {
+ uint8_t s6_addr[16];
+};
+struct sockaddr_in6 {
+ uint8_t sin6_len;
+ sa_family_t sin6_family;
+ in_port_t sin6_port;
+ uint32_t sin6_flowinfo;
+ struct in6_addr sin6_addr;
+};
